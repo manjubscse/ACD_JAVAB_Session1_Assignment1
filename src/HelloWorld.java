56a5af3580c5670b0bf3f905430672d2995050eb
manjubscse <manjubscse@gmail.com>
2017-09-14 00:24:06 +0530

    Suggestion by ACADGILD
    As per suggestion , given the comments for each key word

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,8 +1,14 @@
 
 public class HelloWorld {
 
+//Public is Access Specifier and it is accessable to all the class in the Package.
+//Static is modifier is used for sharing methods and Variables 
+//void is return type -- which does not return any value
+//main is main method , where JVM starts execution from.
+// String[] args - Parameter to main method
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		//System.out.println To Print the Message 
 		System.out.println("Hello World");
 	}
 }
